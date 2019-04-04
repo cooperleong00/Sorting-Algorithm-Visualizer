b265b9efdad0061f28abfd1fef6cbf37ce0dd14d
prismleong <981591636@qq.com>
2019-04-04 11:59:37 +0800

    Update a part of English description

--- AlgorithmExecutor.h
+++ AlgorithmExecutor.h
@@ -8,14 +8,23 @@ public:
 	myVs：指向Visualizer的指针
 	绑定一个Visualizer来根据算法的执行控制矩形的变化
 */
+/*
+	Construction of AlgorithmExecutor
+	myVs：A pointer points to the Visualizer
+	Which binds a Visualizer and controls the displayed rectangles with the running algorithm 
+*/
 	AlgorithmExecutor(Visualizer *myVs);
 /*
 	Execute：子类排序算法必须实现的函数
 	其中实现算法的实行与矩形的控制
 */
+/*
+	Execute：A virtual function which the subclass of the AlgorithmExecutor must implement
+	Implement the exact sorting algorithm and change the status of rectangles
+*/
 	virtual void Execute() = 0 ;
 
-	//绑定的Visualizer
+	//binded Visualizer
 	Visualizer *myVisualizer;
 	//比较的次数
 	size_t CompareTime;
